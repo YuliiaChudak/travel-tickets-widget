05eb4ff28eb79efe8f7e912937d42c2c840370a2
Yuliia Chudak <lifespirit199392@gmail.com>
2021-08-03 22:22:20 +0200

    Add missing exports

--- src/shared/theme/helpers.ts
+++ src/shared/theme/helpers.ts
@@ -3,10 +3,10 @@ import { theme, Theme } from "./theme-configurator";
 type ColorKeys = keyof typeof theme.colors;
 type FontKeys = keyof typeof theme.fonts;
 
-const getColor = (color: ColorKeys) => (props: { theme: Theme }): string => {
+export const getColor = (color: ColorKeys) => (props: { theme: Theme }): string => {
     return props.theme.colors[color];
 };
 
-const getFont = (font: FontKeys) => (props: { theme: Theme }): string => {
+export const getFont = (font: FontKeys) => (props: { theme: Theme }): string => {
     return props.theme.fonts[font];
 };

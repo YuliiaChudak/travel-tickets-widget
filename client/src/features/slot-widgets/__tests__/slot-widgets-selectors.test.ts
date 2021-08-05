48c528748c0a5d0691762e35b77a8a21a144ad68
Yuliia Chudak <lifespirit199392@gmail.com>
2021-08-05 21:50:34 +0200

    Add selectors tests

---
+++ client/src/features/slot-widgets/__tests__/slot-widgets-selectors.test.ts
@@ -0,0 +1,57 @@
+import {
+  getSlotWidgets,
+  getSlotWidgetsError,
+  isFetchingSlotWidgets,
+} from '../slot-widgets-selectors';
+import { initialState, SlotWidgetsState } from '../slot-widgets-reducer';
+import { RootState } from '../../../store';
+import slotWidgets from '../__fixtures__/slot-widgets-get-success.json';
+
+const getMockedStoreValue = (data: Partial<SlotWidgetsState> = {}): RootState =>
+  ({
+    slotWidgets: {
+      ...initialState,
+      ...data,
+    },
+  } as RootState);
+
+describe('Slot widgets selectors', () => {
+  describe('getSlotWidgets', () => {
+    it('returns an empty array', () => {
+      expect(getSlotWidgets(getMockedStoreValue())).toEqual([]);
+    });
+
+    it('returns store slot widgets', () => {
+      expect(
+        getSlotWidgets(
+          getMockedStoreValue({ slots: slotWidgets['slot-widgets'] })
+        )
+      ).toEqual(slotWidgets['slot-widgets']);
+    });
+  });
+
+  describe('getSlotWidgetsError', () => {
+    it('returns null', () => {
+      expect(getSlotWidgetsError(getMockedStoreValue())).toBeNull();
+    });
+
+    it('returns error message', () => {
+      const error = 'Test';
+      expect(getSlotWidgetsError(getMockedStoreValue({ error }))).toEqual(
+        error
+      );
+    });
+  });
+
+  describe('isFetchingSlotWidgets', () => {
+    it('returns false', () => {
+      expect(isFetchingSlotWidgets(getMockedStoreValue())).toBeFalsy();
+    });
+
+    it('returns true', () => {
+      expect(
+        isFetchingSlotWidgets(getMockedStoreValue({ isFetching: true }))
+      ).toBeTruthy();
+    });
+  });
+});

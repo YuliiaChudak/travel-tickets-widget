db7647e5bc4b8de49e49c484a667f9f525a825c9
Yuliia Chudak <lifespirit199392@gmail.com>
2021-08-05 21:50:52 +0200

    Run prettier on thunk

--- client/src/features/slot-widgets/__tests__/slot-widgets-thunks.test.ts
+++ client/src/features/slot-widgets/__tests__/slot-widgets-thunks.test.ts
@@ -2,36 +2,36 @@ import { configureStore } from '@reduxjs/toolkit';
 import { AxiosResponse } from 'axios';
 import * as apiHandlers from '../slot-widgets-api';
 import slotWidgets from '../__fixtures__/slot-widgets-get-success.json';
-import reducer from '../slot-widgets-reducer'
+import reducer from '../slot-widgets-reducer';
 
 import { loadAllSlotWidgets } from '../slot-widgets-thunks';
 
 describe('Slot widget thung', () => {
   describe('loadAllSlotWidgets', () => {
     it('calls an api and fulfill action', async () => {
-      const response = { data: slotWidgets } as AxiosResponse<any>
-        const getAllSlotWidgets = jest
+      const response = { data: slotWidgets } as AxiosResponse<any>;
+      const getAllSlotWidgets = jest
         .spyOn(apiHandlers, 'getAllSlotWidgets')
         .mockResolvedValueOnce(response);
 
-        const store = configureStore({reducer});
-        await store.dispatch(loadAllSlotWidgets());
+      const store = configureStore({ reducer });
+      await store.dispatch(loadAllSlotWidgets());
 
-        expect(getAllSlotWidgets).toHaveBeenCalledTimes(1)
-        expect(store.getState().slots).toEqual(slotWidgets["slot-widgets"])
+      expect(getAllSlotWidgets).toHaveBeenCalledTimes(1);
+      expect(store.getState().slots).toEqual(slotWidgets['slot-widgets']);
     });
 
-      it('calls an api and rejects action', async () => {
-          const error = new Error('Network error!');
-          const getAllSlotWidgets = jest
-              .spyOn(apiHandlers, 'getAllSlotWidgets')
-              .mockRejectedValueOnce(error);
+    it('calls an api and rejects action', async () => {
+      const error = new Error('Network error!');
+      const getAllSlotWidgets = jest
+        .spyOn(apiHandlers, 'getAllSlotWidgets')
+        .mockRejectedValueOnce(error);
 
-          const store = configureStore({reducer});
-          await store.dispatch(loadAllSlotWidgets());
+      const store = configureStore({ reducer });
+      await store.dispatch(loadAllSlotWidgets());
 
-          expect(getAllSlotWidgets).toHaveBeenCalledTimes(1)
-          expect(store.getState().error).toEqual(error.message)
-      });
+      expect(getAllSlotWidgets).toHaveBeenCalledTimes(1);
+      expect(store.getState().error).toEqual(error.message);
+    });
   });
 });

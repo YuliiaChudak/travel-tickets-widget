6930f3c9c4b27e298a57c3731bb0caaf9421258d
Yuliia Chudak <lifespirit199392@gmail.com>
2021-08-06 20:25:26 +0200

    Add box shadow to Box styled component

--- client/src/shared/components/box/Box.styles.ts
+++ client/src/shared/components/box/Box.styles.ts
@@ -5,6 +5,7 @@ import { getColor } from '../../theme/helpers';
 export const Box = styled.div`
   background-color: ${getColor('primaryWhite')};
   border-radius: 10px;
+  box-shadow: 2px 5px 2px rgb(0 0 0 / 6%), 0 2px 0 rgb(0 0 0 / 6%);
   overflow-x: hidden;
   height: 100%;
 `;

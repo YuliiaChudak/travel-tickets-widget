26bfb973a7e30a18bff5f7785c091049ec830498
Yuliia Chudak <lifespirit199392@gmail.com>
2021-08-06 19:26:27 +0200

    Remove unused page component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
 import SlotWidgetScene from './scenes/slot-widgets/SlotWidgetsScene';
-import Page from './shared/components/page/Page';
 import { GlobalStyle } from './shared/theme/global-styles';
 import { NormalizeStyles } from './shared/theme/normalize-styles';
 import { theme } from './shared/theme/theme-configurator';
@@ -16,9 +15,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <NormalizeStyles />
         <GlobalStyle />
-        <Page>
-          <SlotWidgetScene />
-        </Page>
+        <SlotWidgetScene />
       </ThemeProvider>
     </Provider>
   );
--- client/src/shared/components/page/Page.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react';
-
-type Props = {
-  children: ReactElement;
-};
-
-const Page = ({ children }: Props) => <>{children}</>;
-
-export default Page;

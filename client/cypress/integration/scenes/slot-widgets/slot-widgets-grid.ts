aa53783caf75e057f1fb74f1cc31353554c80c84
Yuliia Chudak <lifespirit199392@gmail.com>
2021-08-06 21:50:31 +0200

    Run prettier

--- client/cypress/integration/scenes/slot-widgets/slot-widgets-grid.ts
+++ client/cypress/integration/scenes/slot-widgets/slot-widgets-grid.ts
@@ -1,36 +1,45 @@
-import slotWidgetFixture from '../../../fixtures/slot-widgets/success.json'
-import fareFixture from '../../../fixtures/fares/success.json'
+import slotWidgetFixture from '../../../fixtures/slot-widgets/success.json';
+import fareFixture from '../../../fixtures/fares/success.json';
 
 context('Slot widgets grid', () => {
+  it('displays slot widget correctly', () => {
+    cy.intercept('GET', '**/slot-widgets', {
+      fixture: 'slot-widgets/success.json',
+    }).as('getSlotWidgets');
+    cy.intercept('GET', '**/fares/**', { fixture: 'fares/success.json' }).as(
+      'getFare'
+    );
+    cy.visit('/');
 
-    it('displays slot widget correctly', () => {
-        cy.intercept('GET', '**/slot-widgets', { fixture: 'slot-widgets/success.json' }).as('getSlotWidgets')
-        cy.intercept('GET', '**/fares/**', { fixture: 'fares/success.json' }).as('getFare')
-        cy.visit('/')
+    const [widget] = slotWidgetFixture['slot-widgets'];
+    cy.findByText(widget.bigTitle, { timeout: 1000 }).should('exist');
+    cy.findByText(widget.smallTitle, { timeout: 1000 }).should('exist');
+    cy.findByText(widget.smallTitle, { timeout: 1000 }).should('exist');
+    cy.findByText(fareFixture.fare.time, { timeout: 1000 }).should('exist');
+  });
 
-        const [widget] = slotWidgetFixture["slot-widgets"]
-        cy.findByText(widget.bigTitle, { timeout: 1000 }).should('exist');
-        cy.findByText(widget.smallTitle, { timeout: 1000 }).should('exist');
-        cy.findByText(widget.smallTitle, { timeout: 1000 }).should('exist');
-        cy.findByText(fareFixture.fare.time, { timeout: 1000 }).should('exist');
-    });
+  it('displays an error if fare not loaded', function () {
+    cy.intercept('GET', '**/slot-widgets', {
+      fixture: 'slot-widgets/success.json',
+    }).as('getSlotWidgets');
+    cy.intercept('GET', '**/fares/**', (req) => {
+      req.destroy();
+    }).as('getFare');
+    cy.visit('/');
 
-    it('displays an error if fare not loaded', function () {
-        cy.intercept('GET', '**/slot-widgets', { fixture: 'slot-widgets/success.json' }).as('getSlotWidgets')
-        cy.intercept('GET', '**/fares/**', (req) => {
-            req.destroy();
-        }).as('getFare')
-        cy.visit('/')
+    cy.findByText('Details for this widget are temporary unavailable.', {
+      timeout: 1000,
+    }).should('exist');
+  });
 
-        cy.findByText('Details for this widget are temporary unavailable.', { timeout: 1000 }).should('exist');
-    });
+  it('displays an error if slot widgets are failed to load', () => {
+    cy.intercept('GET', '**/slot-widgets', (req) => {
+      req.destroy();
+    }).as('getFare');
+    cy.visit('/');
 
-    it('displays an error if slot widgets are failed to load', () => {
-        cy.intercept('GET', '**/slot-widgets', (req) => {
-            req.destroy();
-        }).as('getFare')
-        cy.visit('/')
-
-        cy.findByText("Error Network Error occurred. We'll get back to you soon!", { timeout: 1000 }).should('exist');
-    });
+    cy.findByText("Error Network Error occurred. We'll get back to you soon!", {
+      timeout: 1000,
+    }).should('exist');
+  });
 });
